8c150c69722c8c7304f1e7eac0d7537f9245fd47
Tshepiso <timarape.gmail.com>
2021-01-12 06:25:32 +0200

    Generics

---
+++ GenericsApp/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(AreEqual(2,2));
+
+            Console.ReadLine();
+        }
+
+
+
+        static bool AreEqual<T>(T fistNumber, T secondNumber) where T: IComparable<T>
+        {
+
+            return fistNumber.CompareTo(secondNumber) == 0;
+
+        }
+    }
+}
